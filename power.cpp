6ac426f81cc617ef7aece25636f0ca79c7070085
SIVARANJANI G <Ranjaniganesan999@gmail.com>
2017-07-28 12:15:00 +0530

    Create power.cpp

---
+++ power.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+main()
+{
+int a,p;
+cout<<"\nEnter a number:";
+cin>>a;
+cout<<"\nEnter the power:";
+cin>>p;
+cout<<"\n"<<a<<" power "<<p<<" is "<<pow(a,p);
+}
