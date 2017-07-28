d38ee2f40da6ac4d7d3408f133a2ac69a7f625eb
SIVARANJANI G <Ranjaniganesan999@gmail.com>
2017-07-28 11:10:55 +0530

    Create PositiveRNegative.cpp

---
+++ PositiveRNegative.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main()
+{
+int a;
+cout<<"\n Enter a number:";
+cin>>a;
+if(a>0)
+cout<<a<<" is positive\n";
+else if(a<0)
+cout<<a<<" is negative\n";
+else
+cout<<a<<" is neither positive nor negative\n";
+}
